a3633d6de6815dc53ff55f603d23dc8b4ad65692
messyhat <punkd1+1@gmail.com>
2016-07-18 17:28:57 +0800

    initial commit

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+    var counter = 0
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    func startTimer() {
+        counter += 1
+        timerLabel.text = "\(counter)"
+    }
+
+    
+    @IBAction func playButton(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ViewController.startTimer), userInfo: nil, repeats: true)
+    }
+    
+    
+    @IBAction func pauseButton(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
